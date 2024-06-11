6ea043d4b727304eb1fdf7d740d2d7c8190a6706
Atharva262005 <atharvaralegankar2005@gmail.com>
2024-06-11 15:17:38 +0530

    project page fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
 import Projects from './pages/Project';
 
 const App = () => {
     return (
-        <BrowserRouter>
+        
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/home" element={<Home />} />
                 <Route path="/projects" element={<Projects />} />
             </Routes>
-        </BrowserRouter>
     );
 };
 
